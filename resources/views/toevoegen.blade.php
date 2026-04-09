e3dc9a4aef745667f418f91e8214e44efe8e2ef2
emile <338569student@mboutrecht.nl>
2026-04-09 15:23:16 +0200

    type fout

--- resources/views/toevoegen.blade.php
+++ resources/views/toevoegen.blade.php
@@ -37,7 +37,7 @@
         </div>
         
         <div class="form-group">
-            <label>Aanta</label>
+            <label>Aantal</label>
             <input type="number" name="aantal" value="{{ old('aantal') }}" min="0" required>
         </div>
         
